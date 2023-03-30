6f5687b79671126995f38fd65036112a1ad1fe26
ilrobb <roberto.bruno@edu.itspiemonte.it>
2023-03-30 11:23:55 +0200

    divisione e moltiplicazione
    metodo divisione e moltiplicazione, con anche i controlli dell'input

--- calcolatrice.py
+++ calcolatrice.py
@@ -1,1 +1,21 @@
 #calcolatrice
+def controllo_int(testo_scelta):
+    while True:
+        try:
+            intero=int(input(testo_scelta))
+            break
+        except:
+            print("error, not int")
+    return intero
+
+def divisione():
+    y=controllo_int("inscerisci il primo numero")
+    x=controllo_int("inscerisci il secondo numero")
+    somma=y/x
+    return somma
+
+def moltiplicazione():
+    y=controllo_int("inscerisci il primo numero")
+    x=controllo_int("inscerisci il secondo numero")
+    somma=y*x
+    return somma
