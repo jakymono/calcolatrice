c906a3b8206ce49f4a461516bf56adaa451604a8
ilrobb <roberto.bruno@edu.itspiemonte.it>
2023-03-30 11:43:40 +0200

    tolto gli input
    capo ho finito, mi scusi, la prossima volta mi metto in muta

--- calcolatrice.py
+++ calcolatrice.py
@@ -1,21 +1,7 @@
-#calcolatrice
-def controllo_int(testo_scelta):
-    while True:
-        try:
-            intero=int(input(testo_scelta))
-            break
-        except:
-            print("error, not int")
-    return intero
-
 def divisione(x,y):
-    y=controllo_int("inscerisci il primo numero")
-    x=controllo_int("inscerisci il secondo numero")
     somma=y/x
     return somma
 
 def moltiplicazione(x,y):
-    y=controllo_int("inscerisci il primo numero")
-    x=controllo_int("inscerisci il secondo numero")
     somma=y*x
     return somma
